08f1987fdff3292fd9293bd1084e3811145ef47c
IanA <iannaianjos@gmail.com>
2024-09-18 23:17:10 -0300

    Version: 0.1.0 - 18-09-2024 - 23:17

--- examples.py
+++ examples.py
@@ -21,25 +21,25 @@ def regressao_2_regressores(x_1:float, x_2:float, a:float, b:float):
     return x_1*a + x_2*b**2
 
 if __name__ == "__main__":
-##    dado = [[0, 3], [1, 2], [1, 4], [2, 3]]
-##    teste_1 = Regression(regressao_simples)
-##    teste_1.run(dado)
-##    print(teste_1)
-##    
-##    dado = [[x, regressao_simples(x, a = 15, b = 1)] for x in range(10)]
-##    teste_1.run(dado)
-##    print(teste_1)
-##
-##    dado = [[x, regressao_2(x, a = 15, b = -7, c = -4)] for x in range(10)]
-##    teste_2 = Regression(regressao_2)
-##    teste_2.iterations = 100
-##    teste_2.run(dado, precision = 0.0001)
-##    print(teste_2)
-##
-##    dado = [[x, regressao_nao_continua(x, a = 7)] for x in range(20)]
-##    teste_3 = Regression(regressao_nao_continua)
-##    teste_3.run(dado)
-##    print(teste_3)
+    dado = [[0, 3], [1, 2], [1, 4], [2, 3]]
+    teste_1 = Regression(regressao_simples)
+    teste_1.run(dado)
+    print(teste_1)
+    
+    dado = [[x, regressao_simples(x, a = 15, b = 1)] for x in range(10)]
+    teste_1.run(dado)
+    print(teste_1)
+
+    dado = [[x, regressao_2(x, a = 15, b = -7, c = -4)] for x in range(10)]
+    teste_2 = Regression(regressao_2)
+    teste_2.iterations = 100
+    teste_2.run(dado, precision = 0.0001)
+    print(teste_2)
+
+    dado = [[x, regressao_nao_continua(x, a = 7)] for x in range(20)]
+    teste_3 = Regression(regressao_nao_continua)
+    teste_3.run(dado)
+    print(teste_3)
 
     dado = [[x, int(x*3 % 11), regressao_2_regressores(x, int(x*3 % 11), a = 3, b = 10)] for x in range(5)]
     teste_4 = Regression(regressao_2_regressores, regressor = ["x_1", "x_2"])
