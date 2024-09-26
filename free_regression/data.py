722ed36122320bf7388e7c71c3a7e94c996caa5b
IanA <iannaianjos@gmail.com>
2024-09-25 23:30:29 -0300

    Version: 1.0.13 - 25-09-2024 - 23:30

--- free_regression/data.py
+++ free_regression/data.py
@@ -2,6 +2,39 @@
 Dados para testes
 """
 
+def read_csv(csv:str) -> list:
+    """
+    Lê o csv sem precisar usar uma biblioteca
+    """
+    path = f"banco_de_dados/{csv.replace('.csv', '')}.csv"
+    with open(path, "r") as arq:
+        dados = arq.read()
+
+    dados:list = dados.split("\n")[:-1]
+    for line in range(len(dados)):
+        dados[line] = dados[line].split(",")
+
+    return dados
+
+def transpose(data:list) -> list:
+    """
+    Faz a transposição dos dados
+    """
+    new_data:list = []
+    for i in range(len(data[0])):
+        try:
+            new_data.append([data[n][i] for n in range(len(data))])
+        except IndexError:
+            pass
+
+    return new_data
+
+def to_float(number:str) -> float:
+    try:
+        return float(number)
+    except:
+        return number
+
 def to_dummy(data:list) -> list:
     """
     Função que pega uma lista de listas e passa as colunas categoricas para numéricas binárias
@@ -13,15 +46,6 @@ def to_dummy(data:list) -> list:
     assert type(data[0]) == list, "<data[n]> has to be a list"
     assert (k := list(map(len, data))) and max(k) == min(k), "The <data> list must be the same size in all itens"
 
-    def transpose(data:list) -> list:
-        """
-        Faz a transposição dos dados
-        """
-        new_data:list = []
-        for i in range(len(data[0])):
-            new_data.append([data[n][i] for n in range(len(data))])            
-        return new_data
-
     # Transpões os dados
     data = transpose(data)
 
@@ -30,7 +54,7 @@ def to_dummy(data:list) -> list:
     i = 0
     for var in data:
         if str in list(map(type, var)):
-            create.append([i, sorted(list(set(var)))])
+            create.append([i, sorted(list(set(var)))[:-1]])
         i += 1
 
     # Cria as variáveis dummy
@@ -72,29 +96,63 @@ class Representation:
         """
         Ajuda na vizualização para o usuário
         """
-        return f"NOME DO BANCO DE DADOS: {self.name}\nCOLUNAS: {', '.join(self.columns)}"
+        return f"NAME DATA BASE: {self.name}\nCOLUMNS: {', '.join(self.columns)}"
 
-class DataRegression(Representation):
+class MedidasDeMassa(Representation):
     """
     Dados exemplos para teste
     """
+    __socket__ = ("name", "columns", "data", "data_list")
     def __init__(self):
-        self.name:str = ""
-        self.columns:list = ['idade', 'altura']
-        self.data:dict = {"nome_coluna_1":[],
-                          "nome_coluna_2":[],
-                          "nome_coluna_3":[]}
+        # Obtendo e tratando os dados
+        data:list = transpose(read_csv("medidas_de_massa"))
+        columns_use:tuple = (1, 7, 11, 12)
+        data:list = [list(map(to_float, data[i])) for i in columns_use]
+
+        # Criando nome e colunas
+        self.name:str = "Medidas de Massa"
+        self.columns:list = [line[0] for line in data]
+
+        # Dados
+        self.data:dict = {}
+        for i in range(len(data)):
+            self.data[self.columns[i]] = data[i][1:]
+
+        self.data_list:list = transpose([value for value in self.data.values()])
+
+class ProdutividadeTrabalhoRemoto(Representation):
+    """
+    Dados exemplos para teste
+    """
+    __socket__ = ("name", "columns", "data", "data_list")
+    def __init__(self):
+        # Obtendo e tratando os dados
+        data:list = transpose(read_csv("medidas_de_massa"))
+        columns_use:tuple = (1, 7, 11, 12)
+        data:list = [list(map(to_float, data[i])) for i in columns_use]
+
+        # Criando nome e colunas
+        self.name:str = "Medidas de Massa"
+        self.columns:list = [line[0] for line in data]
+
+        # Dados
+        self.data:dict = {}
+        for i in range(len(data)):
+            self.data[self.columns[i]] = data[i][1:]
+
+        self.data_list:list = transpose([value for value in self.data.values()])
 
 if __name__ == "__main__":
-    teste_1 = DataRegression()
-    print(teste_1)
-    #print(teste_1['algo'])
-
+    meus_dados = MedidasDeMassa()
+##    print(teste_1)
+##    #print(teste_1['algo'])
+##
     dados = [[1, "a", 5, "1"],
              [3, "b", 6, "2"],
              [0, "a", 4, "1"],
              [6, "c", 3, "2"],
              [4, "a", 4, "2"],]
 
-    print(to_dummy(dados))
+##
+##    print(to_dummy(dados))
     
