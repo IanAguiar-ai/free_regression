b4d95ee64e4f91aad5c451d31ecde8a6b7d1ad5b
IanA <iannaianjos@gmail.com>
2024-10-06 20:41:06 -0300

    Version: 1.1.2 - 06-10-2024 - 20:41

--- free_regression/__init__.py
+++ free_regression/__init__.py
@@ -2,4 +2,4 @@ from .free_regression import Regression
 from .graphics import plot_expected, plot_residual
 from .models_regression import generate_regression, generate_mlp, generate_mlp_classifier, generate_mlp_semi_classifier, generate_mlp_sigmoid_sum, generate_mlp_normals, generate_distribuction
 from .data import MedidasDeMassa, ProdutividadeTrabalhoRemoto, to_dummy, transpose, normalize
-from make_animation import 
+from make_animation import make_animation
